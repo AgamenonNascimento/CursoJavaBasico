be6159b23c3ae84a1355388b6cb442f1586c3201
agamenon <agamenonnas@gmail.com>
2025-12-06 01:28:03 -0300

    commit Exer01

---
+++ CursoJavaBasico/src/com/agamenon/aula24/labs/Lampada.java
@@ -0,0 +1,14 @@
+package com.agamenon.aula24.labs;
+
+public class Lampada {
+	String modelo;
+	String tensao;
+	int potencia;
+	String cor;
+	String tipoLuz;
+	int garantiaMeses;
+	
+	String[] tipos = new String[5];
+	
+
+}
