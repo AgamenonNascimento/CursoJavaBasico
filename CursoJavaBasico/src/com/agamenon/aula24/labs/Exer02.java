d41c4d6d45a7ed2165fe700088da651b679e26c3
agamenon <agamenonnas@gmail.com>
2025-12-06 01:38:43 -0300

    commit criação class livro

---
+++ CursoJavaBasico/src/com/agamenon/aula24/labs/Exer02.java
@@ -0,0 +1,15 @@
+package com.agamenon.aula24.labs;
+
+public class Exer02 {
+
+	public static void main(String[] args) {
+		Livro livro = new Livro();
+		livro.nome = "Mastering Ext Js";
+		livro.autor ="Loiane Groner";
+		livro.numPaginas=358;
+		livro.isbn="1782164006";
+		
+		System.out.println("Nome do livro: " + livro.nome);
+
+	}
+}
---
+++ CursoJavaBasico/src/com/agamenon/aula24/labs/Livro.java
@@ -0,0 +1,11 @@
+package com.agamenon.aula24.labs;
+
+public class Livro {
+
+		String nome;
+		String autor;
+		int numPaginas;
+		int anoLancamento;
+		String isbn;
+
+}
