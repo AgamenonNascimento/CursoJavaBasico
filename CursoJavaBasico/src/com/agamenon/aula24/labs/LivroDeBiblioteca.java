8cf33d29f2aba81163902cd2b90bb4f435cd04a7
agamenon <agamenonnas@gmail.com>
2025-12-07 02:46:17 -0300

    commit class LivroDeBiblioteca

---
+++ CursoJavaBasico/src/com/agamenon/aula24/labs/LivroDeBiblioteca.java
@@ -0,0 +1,20 @@
+package com.agamenon.aula24.labs;
+
+import java.util.Date;
+
+public class LivroDeBiblioteca {
+
+	public static void main(String[] args) {
+		String nome;
+		String autor;
+		int numPaginas;
+		int anoLancamento;
+		String isbn;
+		boolean emprestado;
+		
+		Date dataEntrega;
+		String emprestadoPara;
+
+	}
+
+}
