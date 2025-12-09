103552da8dcb0d8b087e9eec8625aec51bdef777
agamenon <agamenonnas@gmail.com>
2025-12-09 01:57:14 -0300

    commit class lampada

--- CursoJavaBasico/src/com/agamenon/aula24/labs/Lampada.java
+++ CursoJavaBasico/src/com/agamenon/aula24/labs/Lampada.java
@@ -10,5 +10,9 @@ public class Lampada {
 	
 	String[] tipos = new String[5];
 	
+	
+		
+	}
+	
 
-}
+
---
+++ CursoJavaBasico/src/com/agamenon/aula27/labs/Lampada.java
@@ -0,0 +1,39 @@
+package com.agamenon.aula27.labs;
+
+public class Lampada {
+	String modelo;
+	String tensao;
+	int potencia;
+	String cor;
+	String tipoLuz;
+	int garantiaMeses;
+	boolean ligada;
+	
+	String[] tipos = new String[5];
+	
+	void ligar() {
+		ligada = true;
+	}
+	void desligar() {
+		ligada = false;
+	}
+	void mostrarEstado() {
+		if (ligada) {
+			System.out.println("A lâmpada está ligada");
+		}else {
+			System.out.println("A lâmpada está desligada");
+		}
+	}
+	
+	void mudarEstado() {
+		if(ligada) {
+			desligar();
+		}else {
+			ligar();
+		}
+	}
+		
+
+}
+
+
