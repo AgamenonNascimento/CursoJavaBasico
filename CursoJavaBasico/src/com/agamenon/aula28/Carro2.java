c4fee60817d08c952e9bd686a76106678e34255f
agamenon <agamenonnas@gmail.com>
2025-12-11 03:50:38 -0300

    commit class carro

--- CursoJavaBasico/src/com/agamenon/aula28/Carro2.java
+++ CursoJavaBasico/src/com/agamenon/aula28/Carro2.java
@@ -1,5 +1,25 @@
 package com.agamenon.aula28;
 
 public class Carro2 {
+	String marca;
+	String modelo;
+	int numPassageiros;
+	double capCombustivel;
+	double consumoCombustivel;
+	
+	public Carro2(String marca, String modelo, int numPassageiros, double capCombustivel, double consumoCombustivel) {
+		super();
+		this.marca = marca;
+		this.modelo = modelo;
+		this.numPassageiros = numPassageiros;
+		this.capCombustivel = capCombustivel;
+		this.consumoCombustivel = consumoCombustivel;
+	}
+
+	public Carro2() {
+		super();
+		
+	}
+	
 
 }
