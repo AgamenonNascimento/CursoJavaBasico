9d1c428e7093bfa6bd6c93a17d07474ded123dcc
agamenon <agamenonnas@gmail.com>
2025-12-11 07:18:53 -0300

    commit aula 30

---
+++ CursoJavaBasico/src/com/agamenon/aula30/TesteCarro.java
@@ -0,0 +1,12 @@
+package com.agamenon.aula30;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		Carro van = new Carro("Fiate", "Ducato");
+		
+		System.out.println(van.numPassageiros);
+
+	}
+
+}
