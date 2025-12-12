93ffcde3c8df538769c08125965dd9a195fe562c
agamenon <agamenonnas@gmail.com>
2025-12-12 00:39:48 -0300

    commit aula 31

--- CursoJavaBasico/src/com/agamenon/aula31/Carro.java
+++ CursoJavaBasico/src/com/agamenon/aula31/Carro.java
@@ -1,11 +1,26 @@
 package com.agamenon.aula31;
 
-public class Carro {
+class Carro {
 	
 	String carro;
 	String modelo;
 	int numPassageiros;
 	double capCombustivel;
 	double consumoCombustivel;
+	
+	void exibirAutonomia() {
+		System.out.println("A autonomia do carro é: " + 
+	    this.capCombustivel * this.consumoCombustivel + " km");
+	}
 
-}
+	double obterAutonomia() {
+		return this.capCombustivel * this.consumoCombustivel;
+	}
+
+	double calcularCombustivel(double km) {
+		double qtdCombustivel = km / this.consumoCombustivel;
+		return qtdCombustivel;
+	}
+
+	}
+
---
+++ CursoJavaBasico/src/com/agamenon/aula31/TesteCarro.java
@@ -0,0 +1,9 @@
+package com.agamenon.aula31;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
