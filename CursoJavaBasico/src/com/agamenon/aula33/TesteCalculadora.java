0a6cf35cbefa26b6cdc42e598ab089b9524be8f7
agamenon <agamenonnas@gmail.com>
2025-12-13 05:00:51 -0300

    commit teste aula33

---
+++ CursoJavaBasico/src/com/agamenon/aula33/TesteCalculadora.java
@@ -0,0 +1,17 @@
+package com.agamenon.aula33;
+
+public class TesteCalculadora {
+
+	public static void main(String[] args) {
+		
+		MinhaCalculadora calc = new MinhaCalculadora();
+		double soma;
+		soma =calc.soma(10.5, 5.5);
+		
+		System.out.println(soma);
+		
+		
+
+	}
+
+}
