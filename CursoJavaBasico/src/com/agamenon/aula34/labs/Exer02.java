b12f19aa3e4d56c3ef523ae6344e335d8ed4fc66
agamenon <agamenonnas@gmail.com>
2025-12-14 06:16:08 -0300

    commit exer02

---
+++ CursoJavaBasico/src/com/agamenon/aula34/labs/Exer02.java
@@ -0,0 +1,16 @@
+package com.agamenon.aula34.labs;
+
+public class Exer02 {
+
+	public static void main(String[] args) {
+		
+		imprimirTela(Calculadora.potencia(2, 5));
+		
+
+	}
+	
+	static void imprimirTela(int num) {
+		System.out.println(num);
+	}
+
+}
