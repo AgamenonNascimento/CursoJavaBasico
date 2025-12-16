01f68531516b1143c0e83cee4a4dc67aa9ec6136
agamenon <agamenonnas@gmail.com>
2025-12-16 07:59:47 -0300

    commit class teste fibonacci

---
+++ CursoJavaBasico/src/com/agamenon/aula35/labs/Exerc01.java
@@ -0,0 +1,13 @@
+package com.agamenon.aula35.labs;
+
+public class Exerc01 {
+	
+	public static void main(String[] args) {
+		
+		for (int i=0; i < 14; i++) {
+			System.out.print(Calculadora.fibonacci(i) + " ");
+		}
+		
+	}
+
+}
