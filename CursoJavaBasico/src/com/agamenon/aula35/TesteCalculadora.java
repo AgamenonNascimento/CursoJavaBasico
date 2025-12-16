063587687e2e69e955f7df35165642198112cbee
agamenon <agamenonnas@gmail.com>
2025-12-16 07:43:55 -0300

    commit teste class calculadora

---
+++ CursoJavaBasico/src/com/agamenon/aula35/TesteCalculadora.java
@@ -0,0 +1,16 @@
+package com.agamenon.aula35;
+
+public class TesteCalculadora {
+
+	public static void main(String[] args) {
+		int fatorianNR = Calculadora.fatorialNaoRecursivo(5);
+		System.out.println(fatorianNR);
+		
+		int fatorialR = Calculadora.fatorial(5);
+		System.out.println(fatorialR);
+		
+		
+
+	}
+
+}
