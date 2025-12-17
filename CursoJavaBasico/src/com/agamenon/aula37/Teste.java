151277cd0cb66a1c5015940261347b7116e0f982
agamenon <agamenonnas@gmail.com>
2025-12-17 06:03:57 -0300

    commit class teste aula 37

---
+++ CursoJavaBasico/src/com/agamenon/aula37/Teste.java
@@ -0,0 +1,18 @@
+package com.agamenon.aula37;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Aluno aluno = new Aluno();
+		
+		aluno.setNome("Agamenon");
+		
+		Professor professor = new Professor();
+		professor.setNomeCurso("Logica");
+		
+		professor.setTelefoneCelular("98 9999- 99999");
+
+	}
+
+}
