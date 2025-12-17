38d531ae56010c4dcf6248f8846d77f19381d8ff
agamenon <agamenonnas@gmail.com>
2025-12-17 17:35:51 -0300

    commit class professor

---
+++ CursoJavaBasico/src/com/agamenon/aula42/Professor.java
@@ -0,0 +1,32 @@
+package com.agamenon.aula42;
+
+public class Professor  {
+	private double salario;
+	private String nomeCurso;
+	
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+	
+	public double clacularSalarioLiquido() {
+		return 0;
+	}
+	
+	/*public String obterEiquetaEndereco() {
+		String s = "Nome do professor " + super.getNome() + "\n" ;
+				s += "Endereço : " + super.getEndereco()+ "\n";
+				
+		return s;
+	}*/
+	
+	
+}
