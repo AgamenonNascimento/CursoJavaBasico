8cd50c3b988ccdc697a1781124e631e0b20aacb7
agamenon <agamenonnas@gmail.com>
2025-12-18 18:33:08 -0300

    commit class Animal

---
+++ CursoJavaBasico/src/com/agamenon/aula44/Animal.java
@@ -0,0 +1,23 @@
+package com.agamenon.aula44;
+
+public abstract class Animal {
+	private String nome;
+	
+	public abstract void emitirSom();
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void amamentar() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
