eddc90f296ae3f1286a7116a985a6295c6c49d4f
agamenon <agamenonnas@gmail.com>
2025-12-19 06:57:48 -0300

    commit class Cubo

---
+++ CursoJavaBasico/src/com/agamenon/aula46/labs/Cubo.java
@@ -0,0 +1,15 @@
+package com.agamenon.aula46.labs;
+
+public class Cubo extends Figura3D {
+	
+	private double lado;
+
+	public double getLado() {
+		return lado;
+	}
+
+	public void setLado(double lado) {
+		this.lado = lado;
+	}
+
+}
