5cb6ca35debac63bc06f7e2aa18ae2f9bb27273d
agamenon <agamenonnas@gmail.com>
2025-12-19 08:45:36 -0300

    commit update Figura2D

--- CursoJavaBasico/src/com/agamenon/aula46/labs/Figura2D.java
+++ CursoJavaBasico/src/com/agamenon/aula46/labs/Figura2D.java
@@ -1,5 +1,5 @@
 package com.agamenon.aula46.labs;
 
-public abstract class Figura2D extends FiguraGeometrica {
+public abstract class Figura2D extends FiguraGeometrica implements DimensaoSuperficial {
 
 }
