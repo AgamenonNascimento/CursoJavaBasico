3a55998e416e567369ccb80a2e4dd69c38898f6b
agamenon <agamenonnas@gmail.com>
2025-12-19 08:45:19 -0300

    commit update Figura3D

--- CursoJavaBasico/src/com/agamenon/aula46/labs/Figura3D.java
+++ CursoJavaBasico/src/com/agamenon/aula46/labs/Figura3D.java
@@ -1,5 +1,5 @@
 package com.agamenon.aula46.labs;
 
-public abstract class Figura3D extends FiguraGeometrica{
+public abstract class Figura3D extends FiguraGeometrica implements DimensaoSuperficial, DimensaoVolumetrica{
 
 }
