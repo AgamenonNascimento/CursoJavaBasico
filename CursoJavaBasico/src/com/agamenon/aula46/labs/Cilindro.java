fdccfb600433fe082d9b7d7eb122a471bf634c90
agamenon <agamenonnas@gmail.com>
2025-12-19 06:58:17 -0300

    commit class Cilindro

---
+++ CursoJavaBasico/src/com/agamenon/aula46/labs/Cilindro.java
@@ -0,0 +1,23 @@
+package com.agamenon.aula46.labs;
+
+public class Cilindro extends Figura3D{
+	
+	private double raio;
+	private double altura;
+	
+	public double getRaio() {
+		return raio;
+	}
+	public void setRaio(double raio) {
+		this.raio = raio;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	
+
+}
