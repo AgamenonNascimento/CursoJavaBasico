24ab30489fc75d31480b034c7aa4aef72cff7ce4
agamenon <agamenonnas@gmail.com>
2025-12-19 08:43:25 -0300

    commit update Class Cilindro

--- CursoJavaBasico/src/com/agamenon/aula46/labs/Cilindro.java
+++ CursoJavaBasico/src/com/agamenon/aula46/labs/Cilindro.java
@@ -17,6 +17,18 @@ public class Cilindro extends Figura3D{
 	public void setAltura(double altura) {
 		this.altura = altura;
 	}
+	@Override
+	public double calcularArea() {
+		double areaBase = Math.PI *(raio * raio);
+		double areaLateral = 2 * Math.PI * raio * altura;
+		double areaTotal = (2 * areaBase ) + areaLateral;
+		
+		return areaTotal;
+	}
+	@Override
+	public double calcularVolume() {
+		return (Math.PI *(Math.pow(raio, 2))* altura);
+	}
 	
 	
 
